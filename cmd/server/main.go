574d33a924689ef762d257401d6310b247a92514
prutmongkol <150164864+prutmongkol@users.noreply.github.com>
2025-04-02 21:24:49 +0700

    add RabbitMQ connection with shutdown handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
-	fmt.Println("Starting Peril server...")
+	const rabbitConnString = "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(rabbitConnString)
+	if err != nil {
+		log.Fatalf("failed to connect to RabbitMQ: %v", err)
+	}
+	defer conn.Close()
+	fmt.Println("Connected to RabbitMQ")
+
+	// wait for ctrl+c
+	signalChannel := make(chan os.Signal, 1)
+	signal.Notify(signalChannel, os.Interrupt)
+	<-signalChannel
+	fmt.Println("Shutting down...")
 }
